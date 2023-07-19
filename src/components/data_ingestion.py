cd42201c981e9e82d1240668daf621c32f822959
meghasn <meghoos99@gmail.com>
2023-07-19 22:27:19 +0530

    trained model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 from src.components.data_transformation import DataTransformatioConfig,DataTransformation
 @dataclass#decorator used to define class variables without init
 class DataIngestionConfig:#any input we require will happen through this class
@@ -48,4 +49,6 @@ if __name__ =="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
