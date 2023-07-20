20504e40386fc27f38e54c3c205193f4af00a96f
meghasn <meghoos99@gmail.com>
2023-07-20 21:41:00 +0530

    Setup flask app and prediction pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,47 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+import pickle
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path='artifact/model.pkl'
+            preprocessor_path='artifact/preprocessor.pkl'
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+
+class CustomData:
+    def __init__(self,gender,race_ethnicity,parental_level_of_education,lunch,test_preparation_course,reading_score,writing_score):
+        self.gender=gender
+        self.race=race_ethnicity
+        self.parental_ed=parental_level_of_education
+        self.lunch=lunch
+        self.test_prep=test_preparation_course
+        self.read=reading_score
+        self.write=writing_score
+
+    def get_data_as_frame(self):
+        try:
+            custom_data_input_dict={
+                 "gender": [self.gender],
+                "race_ethnicity": [self.race],
+                "parental_level_of_education": [self.parental_ed],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_prep],
+                "reading_score": [self.read],
+                "writing_score": [self.write],
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e,sys)
